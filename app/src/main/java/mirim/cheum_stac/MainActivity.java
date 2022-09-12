880a17b5b799010807074a1507480be35566b67b
ImJaeYeeon <w2109@e-mirim.hs.kr>
2022-09-13 03:03:01 +0900

    하단탭 이동

--- app/src/main/java/mirim/cheum_stac/MainActivity.java
+++ app/src/main/java/mirim/cheum_stac/MainActivity.java
@@ -1,6 +1,9 @@
 package mirim.cheum_stac;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -8,18 +11,55 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import net.daum.mf.map.api.MapView;
 
 import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private HomeFragment homeFragment = new HomeFragment();
+    private MapFragment mapFragment = new MapFragment();
+    private FillFragment fillFragment = new FillFragment();
+    private MypageFragment mypageFragment = new MypageFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout,homeFragment).commit(); //초기화면 HomeFragment로 지정
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.btn_navi_menu);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()){
+                    case R.id.tab_home:
+                        transaction.replace(R.id.frameLayout,homeFragment).commit();
+                        break;
+                    case R.id.tab_map:
+                        transaction.replace(R.id.frameLayout,mapFragment).commit();
+                        break;
+                    case R.id.tab_fill:
+                        transaction.replace(R.id.frameLayout,fillFragment).commit();
+                        break;
+                    case R.id.tab_myPage:
+                        transaction.replace(R.id.frameLayout,mypageFragment).commit();
+                        break;
+
+                }
+                return false;
+            }
+        });
         gethash(); //키해시 값 구하는 메서드
 
     }
